938ce47628edcc40943e17e233860fa1e9554877
Bohdan2003 <killin22003@gmail.com>
2021-10-06 12:15:16 +0300

    added autoplay scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function(){
    $('.slider-content__items').slick({
         arrows:false,
-        dots:true
+        dots:true,
+        fade:true,
+        autoplay:true,
+        autoplaySpeed:3000
    });
 });
